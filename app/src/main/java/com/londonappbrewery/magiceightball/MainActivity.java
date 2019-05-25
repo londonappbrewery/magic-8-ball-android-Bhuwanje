65a588d09b43544ce011bb53dcbd22948c9c923a
Bhuwanje <becurious56@gmail.com>
2019-05-25 13:23:20 +0545

    project complete..!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button rollButton = (Button) findViewById(R.id.roll_button);
+
+        final ImageView photo = (ImageView) findViewById(R.id.eightball_1);
+        final int [] diceArray =
+                {R.drawable.ball1,
+                        R.drawable.ball2,
+                        R.drawable.ball3,
+                        R.drawable.ball4,
+                        R.drawable.ball5};
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("माइला", "la badhai xa BHUWAN");
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(5);
+                Log.d("माइला", "The random number is:" + number);
+                photo.setImageResource(diceArray[number]);
+            }
+        });
     }
 }
